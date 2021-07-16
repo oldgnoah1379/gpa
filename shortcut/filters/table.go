53993a5eba162b1e4b83e5e97e178f0e260c9740
hoangnh <nguyenhai.hoang@etc.vn>
2021-07-16 13:32:33 +0700

    add table shortcut

--- shortcut/filters/table.go
+++ shortcut/filters/table.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/gnoah1379/gpa"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 	"gorm.io/gorm/schema"
 )
 
@@ -13,5 +14,32 @@ func WithSchema(schema, table schema.Tabler) gpa.Filter {
 	}
 }
 
+func WithPreload(tableName string, cond ...interface{}) gpa.Filter {
+	return func(query *gorm.DB) *gorm.DB {
+		return query.Preload(tableName, cond...)
+	}
+}
 
+func WithPreloadAll() gpa.Filter {
+	return func(query *gorm.DB) *gorm.DB {
+		return query.Preload(clause.Associations)
+	}
+}
 
+func WithJoin(modelName string, args ...interface{}) gpa.Filter {
+	return func(query *gorm.DB) *gorm.DB {
+		return query.Joins(modelName, args...)
+	}
+}
+
+func WithSelect(query string, args ...interface{}) gpa.Filter {
+	return func(query *gorm.DB) *gorm.DB {
+		return query.Select(query, args...)
+	}
+}
+
+func WithOmit(fields ...string) gpa.Filter {
+	return func(query *gorm.DB) *gorm.DB {
+		return query.Omit(fields...)
+	}
+}
